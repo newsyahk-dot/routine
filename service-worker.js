fc063fd90bcf96ac5ab5552121ac91b78c2f3f97
newsyahk-dot <newsyahk@gmail.com>
2026-08-31 14:20:37 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'daily-lap-v3';
+const CACHE_NAME = 'daily-lap-v4';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-512-maskable.png'];
 
 self.addEventListener('install', (e) => {
